421e7dab9240189d5023a9fc37197cb255e132c1
josh long <josh@sonr.io>
2022-04-10 20:13:47 -0400

    feat: addition of storybook configuration files

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+// .storybook/main.js
+
+module.exports = {
+    "stories": [
+      "../src/**/*.stories.mdx",
+    ],
+    "addons": [
+      "@storybook/addon-links",
+      "@storybook/addon-essentials",
+      "@storybook/addon-interactions",
+      "@storybook/preset-create-react-app",
+    ],
+    "framework": "@storybook/react",
+    "core": {
+      "builder": "webpack5"
+    }
+  }
