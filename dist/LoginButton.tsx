01683486d12a35fe3b63dca5406927dfb19944a3
josh long <josh@sonr.io>
2022-04-12 11:52:02 -0400

    dev: Update to loginbutton flow

---
+++ dist/LoginButton.tsx
@@ -0,0 +1,21 @@
+import { loginButtonProps } from './../types/loginButtonProps';
+import {ReactComponent as Logo } from "./../assets/Gray_Inverted.svg";
+import { MouseEventHandler } from 'react';
+
+export function LoginButton(loginButtonProps: loginButtonProps) {
+  function onClickWrapper(callback: () => void): MouseEventHandler<HTMLButtonElement> {
+    return (e: any) => {
+      callback && callback();
+    }
+  } 
+  return (
+    <div className="inline-flex items-center mx-auto pt-8">
+        <button 
+          className={loginButtonProps.styling}
+          onClick={onClickWrapper(loginButtonProps.onLogin)}>
+          <Logo className="w-8 h-8 mr-2" />
+          <span className='text-md pr-8'>{loginButtonProps.label}</span>
+        </button>
+    </div>
+  )
+}
