b67b3dc864f201bc81be673671f589d8f33474d0
josh long <josh@sonr.io>
2022-04-12 11:53:12 -0400

    dev: addition of domain prop in test app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import LoginButton from './components/LoginButton';
+import { LoginButton } from './components/LoginButton';
 
 function App() {
   return (
     <div className="App">
       <div className="flex flex-col items-center mx-auto p-8">
-      <LoginButton 
+      <LoginButton
+      domain="foo" 
       label="Login" 
       styling={"inline-flex items-center px-4 py-2 text-white bg-primaryLight-500 rounded hover:bg-primaryLight-700"} 
       onLogin={() => alert("Login!")} 
