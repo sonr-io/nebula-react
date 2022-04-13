fe57de7a8eeb98d134eccfc55863913daf7a7781
josh long <josh@sonr.io>
2022-04-13 18:28:13 -0400

    feat: addition of cjs and esm rollup output

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,26 @@ import { babel } from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import svgr from '@svgr/rollup';
 import url from '@rollup/plugin-url';
+const packageJson = require('./package.json');
 
 const config = {
   input: './src/components/index.ts',
-  output: {
-    dir: 'output',
-    format: 'cjs'
-  },
+  output:[
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      dir: 'output',
+      format: 'cjs'
+    },
+  ],
   plugins: [babel({ 
       presets: ['@babel/preset-react'], 
       babelHelpers: 'bundled',
