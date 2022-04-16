aabad637ee025de098ef504bc0231515e08f8c37
josh long <josh@sonr.io>
2022-04-16 10:07:43 -0400

    dev: fix to register button

--- src/types/registerButtonProps.ts
+++ src/types/registerButtonProps.ts
@@ -1,5 +1,18 @@
 export interface RegisterButtonProps {
-    label: string;
-    styling: string;
-    onRegister: () => void;
-}
+    domain: string;                         // snr domain naim for authentication
+    label: string;                          // label for the button
+    styling: string;                        // css style string
+    onRegister: AuthenticationResult;          // callback for post authentication steps
+    onError: AuthenticationError;          // callback for error during authentication flow.
+};
+
+/**
+ * Called on the resolve of the authentication operation.
+ * @param result boolean indicating the status of the authentication 
+ */
+export declare type AuthenticationResult = (result: boolean) => void;
+
+/**
+ * Called on error during authentication flow. passes error context to the callback.
+ */
+export declare type AuthenticationError = (error: any) => void;
