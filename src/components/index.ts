dd3778123d5112e71675f2347ad1a7f62dc5037c
josh long <josh@sonr.io>
2022-04-16 10:27:15 -0400

    dev: update to types

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
-export {loginButtonProps} from './../types/index';
-export {registerButtonProps} from './../types/index';
 export {LoginButton} from './LoginButton/index';
 export {RegisterButton} from './RegisterButton/index';
 export {RegisterForm} from './RegisterForm/index';
