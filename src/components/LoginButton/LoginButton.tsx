e1d0fe265161b8f5689fd617bed9c220b8ebab4b
brokecollegedevwithaclothingobsession <supremehitsheads@gmail.com>
2022-05-03 09:46:12 -0700

    added comment to icons and corrected login button

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -1,8 +1,7 @@
-import { AuthenticationError, AuthenticationResult, loginButtonProps } from '../../types/LoginButton/loginButtonProps';
-import { SonrLogoWrapper } from './../Icons';
 import { MouseEventHandler } from 'react';
+import { AuthenticationError, AuthenticationResult, loginButtonProps } from '../../types/LoginButton/loginButtonProps';
 
-const startUserLogin = require('@sonr-io/webauthn').startUserLogin;
+const { startUserLogin } = require('@sonr-io/webauthn');
 
 export function LoginButton(props: loginButtonProps) {
   function onClickWrapper(callback: AuthenticationResult, errorCallback: AuthenticationError): MouseEventHandler<HTMLButtonElement> {
@@ -10,24 +9,25 @@ export function LoginButton(props: loginButtonProps) {
       startUserLogin({
         name: props.domain,
         crossOrigin: false,
-        rpId: 'Sonr'
+        rpId: 'Sonr',
       }).then((result: boolean) => {
         callback && callback(result);
       }).catch((error: any) => {
         errorCallback && errorCallback(error);
       });
-    }
-  };
+    };
+  }
 
   return (
     <div className="inline-flex items-center mx-auto pt-8">
-        <button
-          id='login-button' 
-          className={props.styling}
-          onClick={onClickWrapper(props.onLogin, props.onError)}>
-            {/* <SonrLogoWrapper/> */}
-          <span className='text-md pr-8'>{props.label}</span>
-        </button>
+      <button
+        id="login-button"
+        className={props.styling}
+        onClick={onClickWrapper(props.onLogin, props.onError)}
+      >
+        {/* <SonrLogoWrapper/> */}
+        <span className="text-md pr-8">{props.label}</span>
+      </button>
     </div>
-  )
+  );
 }
