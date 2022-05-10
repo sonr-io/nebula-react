a8b4a82222c3d6738ca37504f52767965b223484
brokecollegedevwithaclothingobsession <supremehitsheads@gmail.com>
2022-05-10 14:39:32 -0700

    test 3 - changing jest to cover src only

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,13 @@ module.exports = async () => ({
   bail: 3,
   clearMocks: true,
   collectCoverageFrom: [
-    '**/*.{js,jsx}',
+    'src/**/*.{ts,tsx}',
   ],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
   coverageReporters: ['json', 'lcov', 'text'],
   coverageDirectory: './coverage',
   verbose: true,
