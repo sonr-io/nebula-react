6c501f43a694a43d94190e5551f93e867a265cac
Guilherme Welter <welter@ae.studio>
2022-06-14 17:31:37 -0300

    Fix import for rollup build

--- src/components/Icons/Info.tsx
+++ src/components/Icons/Info.tsx
@@ -1,7 +1,8 @@
-import cx from 'classnames';
 import Info from "../../assets/Info.svg";
 
 import { IconProps } from "../../types";
+
+const cx = require('classnames');
 
 export function InfoIcon(props: IconProps) {
   const classes = cx('w-4 h-4 mr-2', props.className);
--- src/components/Icons/Person.tsx
+++ src/components/Icons/Person.tsx
@@ -1,7 +1,8 @@
-import cx from 'classnames';
 import Person from "../../assets/Person.svg";
 
 import { IconProps } from "../../types";
+
+const cx = require('classnames');
 
 export function PersonIcon(props: IconProps) {
   const classes = cx('w-4 h-4 mr-2', props.className);
