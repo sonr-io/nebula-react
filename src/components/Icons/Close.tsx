d01123cf1127751eb358f7fd420cfe0c4bc477f6
Guilherme Welter <welter@ae.studio>
2022-06-14 17:34:00 -0300

    Fix import for rollup build

--- src/components/Icons/Close.tsx
+++ src/components/Icons/Close.tsx
@@ -1,7 +1,8 @@
-import cx from 'classnames';
 import Close from "../../assets/Close.svg";
 
 import { IconProps } from "../../types";
+
+const cx = require('classnames');
 
 export function CloseIcon(props: IconProps) {
   const classes = cx('w-4 h-4 mr-2', props.className);
