32342da7510f47f540d35c02a80049f2401b3292
Guilherme Welter <welter@ae.studio>
2022-06-17 11:35:24 -0300

    Add LabelInput tests

--- src/components/LabelInput/LabelInput.test.tsx
+++ src/components/LabelInput/LabelInput.test.tsx
@@ -32,14 +32,66 @@ test("LabelInput should be a function", () => {
   expect(typeof LabelInput).toBe("function");
 });
 
-test("LabelInput should be vertical", () => {
-  const { input, queryByTestId } = setup({ label: 'label', vertical: true });
+test("LabelInput should be vertical with caption", () => {
+  const { input, queryByTestId } = setup({ label: 'label', caption: 'caption', vertical: true });
+  const caption = queryByTestId('nebula-input-caption');
 
   expect(input).toHaveClass('pl-3');
+  expect(caption).toHaveTextContent('caption');
 })
 
-test("LabelInput should be inline (default)", () => {
-  const { input, queryByTestId } = setup({ label: 'label' });
+test("LabelInput should be vertical without caption", () => {
+  const { input, queryByTestId } = setup({ label: 'label', vertical: true });
+  const caption = queryByTestId('nebula-input-caption');
 
   expect(input).toHaveClass('pl-3');
+  expect(caption).toBeFalsy();
+})
+
+test("LabelInput should be vertical with label", () => {
+  const { input, queryByTestId } = setup({ label: 'label', vertical: true });
+  const label = queryByTestId('nebula-input-label');
+
+  expect(input).toHaveClass('pl-3');
+  expect(label).toHaveTextContent('label');
+})
+
+test("LabelInput should be vertical without label", () => {
+  const { input, queryByTestId } = setup({ caption: 'caption', vertical: true });
+  const label = queryByTestId('nebula-input-label');
+
+  expect(input).toHaveClass('pl-3');
+  expect(label).toBeFalsy();
+})
+
+test("LabelInput should be inline with caption", () => {
+  const { input, queryByTestId } = setup({ label: 'label', caption: 'caption' });
+  const caption = queryByTestId('nebula-input-caption');
+
+  expect(input).toHaveClass('pl-3');
+  expect(caption).toHaveTextContent('caption');
+})
+
+test("LabelInput should be inline without caption", () => {
+  const { input, queryByTestId } = setup({ label: 'label' });
+  const caption = queryByTestId('nebula-input-caption');
+
+  expect(input).toHaveClass('pl-3');
+  expect(caption).toBeFalsy();
+})
+
+test("LabelInput should be inline with label", () => {
+  const { input, queryByTestId } = setup({ label: 'label' });
+  const label = queryByTestId('nebula-input-label');
+
+  expect(input).toHaveClass('pl-3');
+  expect(label).toHaveTextContent('label');
+})
+
+test("LabelInput should be inline without label", () => {
+  const { input, queryByTestId } = setup({});
+  const label = queryByTestId('nebula-input-label');
+
+  expect(input).toHaveClass('pl-3');
+  expect(label).toBeFalsy();
 })
--- src/components/LabelInput/LabelInput.tsx
+++ src/components/LabelInput/LabelInput.tsx
@@ -3,15 +3,15 @@ import cx from 'classnames';
 import { Input } from "../Input";
 import { LabelInputProps } from "../../types/inputProps";
 
-const textClasses = "text-gray-600 text-sm"
+const textClasses = "text-input-focused text-sm"
 
 export const LabelInput: React.FC<LabelInputProps> = ({ label, caption, vertical, ...rest }) => {
   return (
     <div className={!vertical ? "flex gap-3" : ''}>
-      {label && <span className={cx(textClasses, { 'mt-2.5': !vertical })}>{label}</span>}
-      <div>
+      {label && <span data-testid="nebula-input-label" className={cx(textClasses, { 'mt-2.5': !vertical })}>{label}</span>}
+      <div className="flex-1">
         <Input {...rest} />
-        {caption && <span className={textClasses}>{caption}</span>}
+        {caption && <span data-testid="nebula-input-caption" className={textClasses}>{caption}</span>}
       </div>
     </div>
   )
