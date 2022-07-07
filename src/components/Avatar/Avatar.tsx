89582918f3a810927b551dd66554b95b743c0b14
Guilherme Welter <welter@ae.studio>
2022-07-07 15:09:13 -0300

    Fix fontsize on Avatar Component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -3,6 +3,12 @@ import { AvatarProps, AvatarSize } from "../../types/avatarProps";
 const cx = require("classnames");
 const defaultClasses = "border rounded-full border-white";
 
+export const fontSize: { [key in AvatarSize]: string } = {
+  lg: 'text-2xl',
+  md: 'text-xl',
+  sm: 'text-base',
+}
+
 export const lineHeight: { [key in AvatarSize]: string } = {
   lg: 'leading-10 pt-1',
   md: 'leading-10',
@@ -17,7 +23,7 @@ export const avatarSizes: { [key in AvatarSize]: string } = {
 
 export const Avatar: React.FC<AvatarProps> = ({ style, src, alt, size = "md", online }) => {
   const containerClasses = cx(defaultClasses, avatarSizes[size], { 'shadow-avatar': online });
-  const altClasses = cx("h-full w-full rounded-full bg-gray-300 text-center align-middle", lineHeight[size]);
+  const altClasses = cx("h-full w-full rounded-full bg-gray-300 text-center align-middle", fontSize[size], lineHeight[size]);
 
   return (
     <div data-testid="nebula-avatar" className={containerClasses} style={style}>
