52750e548148fac07289ac39a7f22fbe33dc9398
Guilherme Welter <welter@ae.studio>
2022-07-29 11:51:51 -0300

    Fix SwitchButton alignment

--- src/components/SwitchButton/SwitchButton.tsx
+++ src/components/SwitchButton/SwitchButton.tsx
@@ -17,13 +17,13 @@ export const SwitchButton: React.FC<SwitchButtonProps> = ({ initialValue, iconNa
 
   const containerClasses = useMemo(() => {
     return cx(
-      'w-14 h-8 rounded-full p-0.5 cursor-pointer',
+      'overflow-hidden w-14 h-8 rounded-full p-0.5 cursor-pointer',
       on ? 'bg-primary' : 'bg-gray-700',
       { 'opacity-50 cursor-not-allowed': disabled }
     )
   }, [on, disabled]);
 
-  const contentClasses = useMemo(() => cx('w-7 h-7 bg-white rounded-full flex items-center justify-center', { 'ml-6': on }), [on]);
+  const contentClasses = useMemo(() => cx('w-7 h-7 bg-white rounded-full flex items-center justify-center', { 'ml-auto': on }), [on]);
   const iconClasses = useMemo(() => on ? 'fill-primary' : 'fill-gray-700', [on]);
 
   const handleSwitch = useCallback(() => {
