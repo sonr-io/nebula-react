15c0b46677ac3bb2d04aff0f0e26931259c0e549
Guilherme André Welter <welter@ae.studio>
2022-07-29 18:33:38 -0300

    Feature/chips (#153)
    * WIP Chip component
    
    * WIP Chip component
    
    * Add Chip to Storybook Index
    
    * Update Chip desciption on storybook
    
    * Remove flex class from Chip

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,22 @@
+import React, { useMemo } from 'react';
+import { ChipProps, ChipSkin } from '../../types/chipProps';
+import { NebulaIcon } from "../Icons/NebulaIcon";
+
+const cx = require('classnames');
+
+export const chipsSkins: { [key in ChipSkin]: string } = {
+  default: 'bg-skin-subtle-inverted bg-opacity-10 text-skin-subtle-inverted',
+  primary: 'bg-blue-700 text-white',
+  critical: 'bg-red-700 text-white',
+}
+
+export const Chip: React.FC<ChipProps> = ({ skin = 'default', text, onRemove }) => {
+  const chipClasses = useMemo(() => cx('rounded w-fit px-2 py-1.5', chipsSkins[skin]), [skin]);
+
+  return (
+    <div className={chipClasses}>
+      <span>{text}</span>
+      {onRemove && <span className='cursor-pointer' onClick={onRemove}><NebulaIcon iconName='Close' iconType='outline' className='ml-2 fill-current' /></span>}
+    </div>
+  )
+}
